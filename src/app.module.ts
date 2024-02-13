45e3baaa58ff6784fb5857864856b5795c96fe3d
Faizan <127212994+CodeSprint01@users.noreply.github.com>
2024-02-13 17:21:58 +0500

    boiler_code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+  }),
+  TypeOrmModule.forRoot({
+
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    database: 'muttaqi',
+    password: '',
+    username: 'root',
+    entities: ['dist/**/*.entity{.ts, .js}'],
+    synchronize: true,
+    autoLoadEntities: true,
+  }),
+
+UserModule
+
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
