f11cef58aa964f1d5d18fdec49b01778f534e199
CodeSprint01 <127212994+CodeSprint01@users.noreply.github.com>
2024-03-18 10:36:20 +0500

    create a module

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
 import { GeneralInformationModule } from './general-information/general-information.module';
 import { FamilyMembersModule } from './family-members/family-members.module';
 import { offeredPrayerModule } from './prayer/prayer.module';
+import { FavouriteModule } from './favourite/favourite.module';
 
 @Module({
   imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
@@ -42,6 +43,9 @@ import { offeredPrayerModule } from './prayer/prayer.module';
 
 
     offeredPrayerModule,
+
+
+    FavouriteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
