44ac8b7366b5b8027788c6b31f8e97544c34c19b
CodeSprint01 <127212994+CodeSprint01@users.noreply.github.com>
2024-03-25 14:27:57 +0500

    fix export issue

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -4,10 +4,13 @@ import { TaskResolver } from './task.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './entities/task.entity';
 import { TaskCategory } from './entities/task-category.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task, TaskCategory])],
+  imports: [TypeOrmModule.forFeature([Task, TaskCategory]),
+UserModule
+],
   providers: [TaskResolver, TaskService],
-  exports: [TaskService]
+  exports: [TaskModule]
 })
 export class TaskModule {}
