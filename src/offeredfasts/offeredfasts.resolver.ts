5ee8e27bd7ce1a9ec8ab0638b1c8d5489282b493
CodeSprint01 <127212994+CodeSprint01@users.noreply.github.com>
2024-03-26 10:00:49 +0500

    add mutation heading

--- src/offeredfasts/offeredfasts.resolver.ts
+++ src/offeredfasts/offeredfasts.resolver.ts
@@ -6,9 +6,9 @@ import { UpdateOfferedfastInput } from './dto/update-offeredfast.input';
 
 @Resolver(() => Offeredfast)
 export class OfferedfastsResolver {
-  constructor(private readonly offeredfastsService: OfferedfastsService) {}
+  constructor(private readonly offeredfastsService: OfferedfastsService) { }
 
-// create offeredFast
+  // create offeredFast
   @Mutation(() => Offeredfast)
   createOfferedfast(@Args('createOfferedfastInput') createOfferedfastInput: CreateOfferedfastInput) {
     return this.offeredfastsService.create(createOfferedfastInput);
@@ -19,18 +19,20 @@ export class OfferedfastsResolver {
   findAll() {
     return this.offeredfastsService.findAll();
   }
- 
+
   //findOne offeredfast
   @Query(() => Offeredfast, { name: 'offeredfast' })
   findOne(@Args('id') id: number): Promise<Offeredfast> {
     return this.offeredfastsService.findOne(id);
   }
 
+  // update offered fast
   @Mutation(() => Offeredfast)
   updateOfferedfast(@Args('updateOfferedfastInput') updateOfferedfastInput: UpdateOfferedfastInput) {
     return this.offeredfastsService.update(updateOfferedfastInput.id, updateOfferedfastInput);
   }
 
+  // remove offered fast 
   @Mutation(() => Offeredfast)
   removeOfferedfast(@Args('id', { type: () => Int }) id: number) {
     return this.offeredfastsService.remove(id);
